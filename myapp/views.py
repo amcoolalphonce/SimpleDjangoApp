d8778a973ad1c490e7d225b05500c9b1dd78e2ad
Amcool <Alphonceomondi273@gmail.com>
2024-02-18 22:29:54 +0300

    login view

--- myapp/views.py
+++ myapp/views.py
@@ -8,3 +8,7 @@ def index(request):
  
 def about(request):
         return render(request, 'myapp/about.html')
+
+#authentivation views
+def login_user(request):
+        return render(request,'authenticaton/login.html' , {})
